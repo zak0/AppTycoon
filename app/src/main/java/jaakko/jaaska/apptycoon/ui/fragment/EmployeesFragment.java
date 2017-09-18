efc5746b49c52a1b80f8fbd2835f41a6000ece8d
Jaakko Jääskä <jaakko.jaaska@gmail.com>
2017-09-18 19:27:03 +0300

    UI enhancements & ripple effect for HR RecyclerView

--- app/src/main/java/jaakko/jaaska/apptycoon/ui/fragment/EmployeesFragment.java
+++ app/src/main/java/jaakko/jaaska/apptycoon/ui/fragment/EmployeesFragment.java
@@ -15,8 +15,8 @@ import android.widget.Toast;
 
 import java.util.List;
 
-import jaakko.jaaska.apptycoon.R;
 import jaakko.jaaska.apptycoon.AppTycoonApp;
+import jaakko.jaaska.apptycoon.R;
 import jaakko.jaaska.apptycoon.engine.core.GameEngine;
 import jaakko.jaaska.apptycoon.engine.core.GameState;
 import jaakko.jaaska.apptycoon.engine.people.EmployeeType;
@@ -50,7 +50,6 @@ public class EmployeesFragment extends Fragment {
         });
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewEmployees);
-        GameState gameState = GameEngine.getInstance().getGameState();
 
         // Show all employee types here (both that company already has and the ones that it doesn't).
         mRecyclerViewAdapter = new EmployeeRecyclerViewAdapter(EmployeeType.getAllTypes());
@@ -62,7 +61,10 @@ public class EmployeesFragment extends Fragment {
         return view;
     }
 
-    private class EmployeeRecyclerViewAdapter extends RecyclerView.Adapter<EmployeeRecyclerViewAdapter.ViewHolder> {
+    private class EmployeeRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        private static final int ITEM_TYPE_LOCKED = 1;
+        private static final int ITEM_TYPE_UNLOCKED = 2;
 
         private List<EmployeeType> mEmployees;
         private GameState mGameState;
@@ -73,28 +75,64 @@ public class EmployeesFragment extends Fragment {
         }
 
         @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_employee, parent, false);
-            ViewHolder viewHolder = new ViewHolder(view);
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-            viewHolder.containerView = view;
+            if (viewType == ITEM_TYPE_UNLOCKED) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_employee, parent, false);
+                UnlockedEmployeeViewHolder viewHolder = new UnlockedEmployeeViewHolder(view);
 
-            viewHolder.layoutEmployeeStats = view.findViewById(R.id.layoutEmployeeStats);
-            viewHolder.textViewEmployeeType = (TextView) view.findViewById(R.id.textViewEmployeeType);
-            viewHolder.textViewEmployeeCount = (TextView) view.findViewById(R.id.textViewEmployeeCount);
-            viewHolder.textViewEmployeeDescription = (TextView) view.findViewById(R.id.textViewEmployeeDescription);
-            viewHolder.textViewEmployeeCodeGain = (TextView) view.findViewById(R.id.textViewEmployeeCodeGain);
-            viewHolder.textViewEmployeeQualityGain = (TextView) view.findViewById(R.id.textViewEmployeeQualityGain);
-            viewHolder.textViewEmployeeSalary = (TextView) view.findViewById(R.id.textViewEmployeeSalary);
+                viewHolder.textViewEmployeeType = (TextView) view.findViewById(R.id.textViewEmployeeType);
+                viewHolder.textViewEmployeeCount = (TextView) view.findViewById(R.id.textViewEmployeeCount);
+                viewHolder.textViewEmployeeDescription = (TextView) view.findViewById(R.id.textViewEmployeeDescription);
+                viewHolder.textViewEmployeeCodeGain = (TextView) view.findViewById(R.id.textViewEmployeeCodeGain);
+                viewHolder.textViewEmployeeQualityGain = (TextView) view.findViewById(R.id.textViewEmployeeQualityGain);
+                viewHolder.textViewEmployeeSalary = (TextView) view.findViewById(R.id.textViewEmployeeSalary);
 
-            return viewHolder;
+                return viewHolder;
+
+            } else { // viewType == ITEM_TYPE_LOCKED
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_employee_locked, parent, false);
+                LockedEmployeeViewHolder viewHolder = new LockedEmployeeViewHolder(view);
+
+                viewHolder.textViewEmployeeType = (TextView) view.findViewById(R.id.textViewEmployeeType);
+                viewHolder.textViewUnlockConditions = (TextView) view.findViewById(R.id.textViewEmployeeUnlockConditions);
+
+                return viewHolder;
+            }
         }
 
         @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
+        public int getItemViewType(int position) {
+            return mEmployees.get(position).isUnlocked() ? ITEM_TYPE_UNLOCKED : ITEM_TYPE_LOCKED;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder vh, int position) {
             final EmployeeType employeeType = mEmployees.get(position);
 
+            if (getItemViewType(position) == ITEM_TYPE_LOCKED) {
+                LockedEmployeeViewHolder holder = (LockedEmployeeViewHolder) vh;
+                holder.textViewEmployeeType.setText(employeeType.getTitle());
+                holder.textViewUnlockConditions.setText(employeeType.getConditionsString());
+
+            } else { // viewType == ITEM_TYPE_UNLOCKED
+                UnlockedEmployeeViewHolder holder = (UnlockedEmployeeViewHolder) vh;
+
+                // EmployeeType needs to be set to holder in order to have access to it
+                // in the OnClickListener set to the card.
+                holder.employeeType = employeeType;
+                refreshEmployeeCard(holder);
+            }
+        }
+
+        /**
+         * Refreshes the views within a given {@link UnlockedEmployeeViewHolder} to correspond
+         * with the current state of the {@link EmployeeType} the card is showing.
+         * @param holder {@link UnlockedEmployeeViewHolder} that needs to be refreshed
+         */
+        private void refreshEmployeeCard(UnlockedEmployeeViewHolder holder) {
             Resources res = AppTycoonApp.getContext().getResources();
+            EmployeeType employeeType = holder.employeeType;
 
             String stringPayrollCount = res.getString(R.string.employee_list_item_n_on_payroll, employeeType.getCount());
             String stringCodeGain = res.getString(R.string.employee_list_item_code_gain, employeeType.getCpsGain());
@@ -104,23 +142,41 @@ public class EmployeesFragment extends Fragment {
             holder.textViewEmployeeType.setText(employeeType.getTitle());
             holder.textViewEmployeeCount.setText(stringPayrollCount);
 
-            if (employeeType.isUnlocked()) {
+            holder.textViewEmployeeDescription.setText(employeeType.getDescription());
 
-                holder.textViewEmployeeDescription.setText(employeeType.getDescription());
+            holder.textViewEmployeeCodeGain.setText(stringCodeGain);
+            holder.textViewEmployeeQualityGain.setText(stringQualityGain);
+            holder.textViewEmployeeSalary.setText(stringSalary);
+        }
 
-                holder.layoutEmployeeStats.setVisibility(View.VISIBLE);
-                holder.textViewEmployeeCodeGain.setText(stringCodeGain);
-                holder.textViewEmployeeQualityGain.setText(stringQualityGain);
-                holder.textViewEmployeeSalary.setText(stringSalary);
+        @Override
+        public int getItemCount() {
+            return mEmployees.size();
+        }
+
+        class UnlockedEmployeeViewHolder extends RecyclerView.ViewHolder {
+
+            EmployeeType employeeType;
+
+            TextView textViewEmployeeType;
+            TextView textViewEmployeeCount;
+            TextView textViewEmployeeDescription;
+            TextView textViewEmployeeCodeGain;
+            TextView textViewEmployeeQualityGain;
+            TextView textViewEmployeeSalary;
+
+            UnlockedEmployeeViewHolder(View itemView) {
+                super(itemView);
 
                 // Now clicking the card hires one more of the selected employee.
-                holder.containerView.setOnClickListener(new View.OnClickListener() {
+                itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         boolean success = mGameState.getCompany().addEmployee(employeeType.getType(), 1);
 
                         if (success) {
-                            mRecyclerViewAdapter.notifyDataSetChanged();
+                            refreshEmployeeCard(UnlockedEmployeeViewHolder.this);
+                            //mRecyclerViewAdapter.notifyDataSetChanged();
                         } else {
                             Toast.makeText(getContext(),
                                     "Office is full. Consider moving into a larger office.",
@@ -130,39 +186,24 @@ public class EmployeesFragment extends Fragment {
                 });
 
                 // Long-clicking fires an employee.
-                holder.containerView.setOnLongClickListener(new View.OnLongClickListener() {
+                itemView.setOnLongClickListener(new View.OnLongClickListener() {
                     @Override
                     public boolean onLongClick(View v) {
                         mGameState.getCompany().removeEmployee(employeeType.getType(), 1);
-                        mRecyclerViewAdapter.notifyDataSetChanged();
+                        refreshEmployeeCard(UnlockedEmployeeViewHolder.this);
                         return true; // do not pass to OnClickListener after this
                     }
                 });
-            } else {
-                holder.textViewEmployeeDescription.setText(employeeType.getConditionsString());
-                holder.layoutEmployeeStats.setVisibility(View.INVISIBLE);
             }
 
         }
 
-        @Override
-        public int getItemCount() {
-            return mEmployees.size();
-        }
-
-        class ViewHolder extends RecyclerView.ViewHolder {
-
-            View containerView;
-            View layoutEmployeeStats;
+        class LockedEmployeeViewHolder extends RecyclerView.ViewHolder {
 
             TextView textViewEmployeeType;
-            TextView textViewEmployeeCount;
-            TextView textViewEmployeeDescription;
-            TextView textViewEmployeeCodeGain;
-            TextView textViewEmployeeQualityGain;
-            TextView textViewEmployeeSalary;
+            TextView textViewUnlockConditions;
 
-            private ViewHolder(View itemView) {
+            LockedEmployeeViewHolder(View itemView) {
                 super(itemView);
             }
         }
